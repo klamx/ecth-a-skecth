e9f035af75db915c436521b6fc05b566e5b7b1f8
Alejandro <largoalejo@gmail.com>
2021-07-01 11:39:17 -0500

    Add options aside

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import './style.css';
 
+let color = '#16161a';
+let grid = 30;
+
 const container = (grid) => {
   const cont = document.querySelector('.container');
   cont.style.cssText = `display: grid; grid-template-columns: repeat(${grid}, calc(90vh / ${grid})); grid-template-rows: repeat(${grid}, calc(90vh / ${grid}));`;
@@ -10,6 +13,7 @@ export const divs = (grid) => {
   for (let i = 0; i < grid * grid; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
+    div.classList.add('toggle');
     div.setAttribute('id', i);
     div.style.cssText = `width: calc(90vh / ${grid}); height: calc(90vh / ${grid});`;
     container.appendChild(div);
@@ -24,7 +28,8 @@ const run = (grid) => {
   items.forEach((div) => {
     addEventListener('mouseover', (e) => {
       if (e.target.id === div.getAttribute('id')) {
-        div.classList.add('hover');
+        // div.classList.add('hover');
+        div.style.cssText = `background: ${color}`;
       }
     });
   });
@@ -32,16 +37,40 @@ const run = (grid) => {
 
 run(30);
 
-const button = document.querySelector('#clear');
-button.addEventListener('click', () => {
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
   const items = document.querySelectorAll('.grid-item');
   items.forEach((item) => item.remove());
-  let grid = 0;
-  try {
-    let newGrid = prompt('Type the width of the new grid: ', 30);
-    grid = newGrid > 0 && newGrid <= 100 ? newGrid : 16;
-  } catch (error) {
-    alert('The width of the grid have to be <= 100');
-  }
   run(grid);
+  const colorP = document.querySelector('#color');
+  colorP.value = '#16161a';
+  color = colorP.value;
 });
+
+const toggle = document.querySelector('#toggle-grid');
+toggle.addEventListener('click', () => {
+  const divs = document.querySelectorAll('.grid-item');
+  divs.forEach((div) => div.classList.toggle('toggle'));
+});
+
+const colorP = document.querySelector('#color');
+colorP.addEventListener(
+  'input',
+  () => {
+    let col = colorP.value;
+    color = col;
+  },
+  false
+);
+
+const range = document.querySelector('#range');
+range.addEventListener(
+  'input',
+  () => {
+    let val = range.value;
+    grid = val;
+    const pRange = document.querySelector('.reset-p');
+    pRange.textContent = `Size: ${val} x ${val}`;
+  },
+  false
+);
